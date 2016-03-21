6707340821cb73b35500a8d18277fa529aaf9984
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-21 04:07:19 -0700

    Create about.lua

---
+++ plugins/about.lua
@@ -0,0 +1,14 @@
+do
+    local function run(msg, matches)
+        return " DeaDKinGs\nAdvanced Bot Base On Seed\n\n@danyyyx[DeVeLoPeR]\n\n@KinG0fDeaD[DeVeLoPeR&CREATOR]\n\n#WE_ARE_WITH_YOU_ON [@DeaD_T34M]"
+    end
+    return {
+        usage = {
+            "kings: About Current Bot.",
+            },
+        patterns = {
+            "^([Kk]ings)$",
+            },
+        run = run 
+    }
+end
